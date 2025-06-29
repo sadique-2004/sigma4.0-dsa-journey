da1cbeca2577ecd00f7fc90d98f3dd26d473aa78
sadique-2004 <mds044234@gmail.com>
2025-06-29 15:14:11 +0530

    Add OOPS concepts examples in Java all code in readMe+in seprate file as well

---
+++ Core_JAVA/OOPs/Start.java
@@ -0,0 +1,33 @@
+package OOPs;
+// class and obj
+
+public class Start {
+    public static void main(String[] args) {
+        Person P1 = new Person();
+        P1.name = "Sadique";
+        P1.age = 25;
+
+        System.out.println(P1.name +" " +P1.age);
+        P1.eat();
+        // P1.walk();
+        P1.walk(100);
+        
+    }
+}
+
+class Person {
+     String name;
+     int age;
+
+     void walk(){
+        System.out.println(name + " is walking");
+     }
+
+     void eat(){
+        System.out.println(name + " is eating");
+     }
+
+     void walk(int steps){
+System.out.println(name + " is walking " + steps + " steps");
+     }
+}
