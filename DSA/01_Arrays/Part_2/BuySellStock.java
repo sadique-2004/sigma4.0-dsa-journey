1c34d5e62a52c2637c539fd3de0e9d112d350658
sadique-2004 <mds044234@gmail.com>
2025-09-03 22:29:40 +0530

    day 1 done

---
+++ DSA/01_Arrays/Part_2/BuySellStock.java
@@ -0,0 +1,5 @@
+public class BuySellStock {
+    public static void main(String[] args) {
+        
+    }
+}
--- DSA/01_Arrays/Part_2/prefixMSA.java
+++ DSA/01_Arrays/Part_2/prefixMSA.java
@@ -1,11 +1,42 @@
 
-
 public class prefixMSA {
 
-    public void prefixMSA() {
-        
+    public static void prefixmaxsum(int[] array) {
+
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+
+        int[] prefixSum = new int[array.length];
+
+        // calculating prefix sum in the array
+
+        prefixSum[0] = array[0];
+        for (int i = 1; i < prefixSum.length; i++) {
+            prefixSum[i] = prefixSum[i - 1] + array[i];
+        }
+
+        for (int i = 0; i < array.length; i++) {
+
+            for (int j = i + 1; j < array.length; j++) {
+
+                currentSum = i == 0 ? prefixSum[j] : prefixSum[j] - prefixSum[i-1];
+
+                if (maxSum < currentSum) {
+                    maxSum = currentSum;
+                }
+
+            }
+
+        }
+
+        System.out.println("Max sub array sum is : "+maxSum);
+
     }
-   public static void main(String[] args) {
-    int[] arr = {1, 2, 3, 4, 5};
-   }
+
+    public static void main(String[] args) {
+        int[] array = { 1, -2, 6, -1, 3 };
+
+        prefixmaxsum(array);
+
+    }
 }
